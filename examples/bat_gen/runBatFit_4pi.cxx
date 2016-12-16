548818cfcc73f54c629a5530b846471c6f37eac4
Johannes Rauch <j.rauch@tum.de>
2016-12-16 11:33:46 +0100

    improve loading of data

--- examples/bat_gen/runBatFit_4pi.cxx
+++ examples/bat_gen/runBatFit_4pi.cxx
@@ -38,37 +38,42 @@ int main()
     // create model
     bat_fit m(d4pi_fit(model_name + "_fit"));
 
+    const unsigned nData = 200000; // number of Data points we want
+
     // real data
     {
         TChain t("t");
         t.Add("/nfs/hicran/scratch/user/jrauch/CopiedFromKEK/DataSkim_0?_analysis.root");
         t.AddFriend("t", "/nfs/hicran/scratch/user/jrauch/CopiedFromKEK/DataSkim_analysis_TMVA_weights.root");
         LOG(INFO) << "Load data";
-        load_data_4pi(m.fitData(), t, 300, BDT_cut, K0_cut, false);
+        load_data_4pi(m.fitData(), t, nData, BDT_cut, K0_cut, false);
         m.fitPartitions() = yap::DataPartitionBlock::create(m.fitData(), 4);
     }
-
-    // MC data
-    {
+    // MC data, load from several streams if needed
+    for (int i = 0; i < 6; ++i) {
+        auto nLoaded = m.integralData().size();
+        if (nLoaded >= nData)
+            break;
         TChain t_mcmc("t");
-        t_mcmc.Add("/nfs/hicran/scratch/user/jrauch/CopiedFromKEK/FourPionsSkim_s0_analysis.root");
-        t_mcmc.AddFriend("t", "/nfs/hicran/scratch/user/jrauch/CopiedFromKEK/FourPionsSkim_analysis_s0_TMVA_weights.root");
+        t_mcmc.Add(TString::Format("/nfs/hicran/scratch/user/jrauch/CopiedFromKEK/FourPionsSkim_s%d_analysis.root", i));
+        t_mcmc.AddFriend("t", TString::Format("/nfs/hicran/scratch/user/jrauch/CopiedFromKEK/FourPionsSkim_analysis_s%d_TMVA_weights.root", i));
         LOG(INFO) << "Load integration (MC) data";
-        load_data_4pi(m.integralData(), t_mcmc, 300, BDT_cut, K0_cut, true);
-        m.integralPartitions() = yap::DataPartitionBlock::create(m.integralData(), 4);
+        load_data_4pi(m.integralData(), t_mcmc, nData-nLoaded, BDT_cut, K0_cut, true);
     }
+    m.integralPartitions() = yap::DataPartitionBlock::create(m.integralData(), 4);
+
 
     // open log file
     BCLog::OpenLog("output/" + m.GetSafeName() + "_log.txt", BCLog::detail, BCLog::detail);
 
     // set precision
     m.SetPrecision(BCEngineMCMC::kMedium);
-    m.SetNIterationsPreRunMax(1e5);
+    m.SetNIterationsPreRunMax(1e6);
     m.SetNChains(4);
     // m.SetMinimumEfficiency(0.85);
     // m.SetMaximumEfficiency(0.99);
 
-    m.SetNIterationsRun(static_cast<int>(1e5 / m.GetNChains()));
+    m.SetNIterationsRun(static_cast<int>(1e6 / m.GetNChains()));
 
     // start timing:
     const auto start = std::chrono::steady_clock::now();
@@ -79,9 +84,16 @@ int main()
     // end timing
     const auto end = std::chrono::steady_clock::now();
 
+    LOG(INFO) << "Find global mode";
     m.FindMode(m.GetBestFitParameters());
 
+    // delete data
+    m.fitData().clear();
+    m.integralData().clear();
+
+    LOG(INFO) << "Print Summary";
     m.PrintSummary();
+    LOG(INFO) << "Generate plots";
     m.PrintAllMarginalized("output/" + m.GetSafeName() + "_plots.pdf", 2, 2);
 
     // timing:
